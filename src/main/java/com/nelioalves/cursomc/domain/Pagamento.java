ef319bb14c825627875447da4b784e34af2f811c
nkredd <nel2703@gmail.com>
2019-02-26 11:43:18 -0300

    Inserindo Pedido

--- src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
@@ -10,19 +10,20 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
 public abstract class Pagamento implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
 	private Integer id;
 	private Integer estado;
-	
+
 	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
@@ -36,7 +37,7 @@ public abstract class Pagamento implements Serializable {
 	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
 		super();
 		this.id = id;
-		this.estado = (estado == null)? null : estado.getCod();
+		this.estado = (estado == null) ? null : estado.getCod();
 		this.pedido = pedido;
 	}
 
@@ -88,5 +89,4 @@ public abstract class Pagamento implements Serializable {
 			return false;
 		return true;
 	}
-
 }
--- src/main/java/com/nelioalves/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.nelioalves.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	private Integer numeroDeParcelas;
@@ -25,5 +27,4 @@ public class PagamentoComCartao extends Pagamento {
 	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
 		this.numeroDeParcelas = numeroDeParcelas;
 	}
-
 }
