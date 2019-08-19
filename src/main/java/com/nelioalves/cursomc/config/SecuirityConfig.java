c918fdcb36509ed83d3c2713af6c80421cad821a
nkredd <nel2703@gmail.com>
2019-08-19 16:24:51 -0300

    Implementando autenticacao e geracao do token JWT

--- src/main/java/com/nelioalves/cursomc/config/SecuirityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecuirityConfig.java
@@ -7,14 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.nelioalves.cursomc.security.JWTAuthenticationFilter;
+import com.nelioalves.cursomc.security.JWTUtil;
 
 
 @Configuration
@@ -23,6 +28,12 @@ public class SecuirityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 
 	public static final String[] PUBLIC_MATCHES = { 
 			"/h2-console/**"			
@@ -47,8 +58,13 @@ public class SecuirityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHES_GET).permitAll()
 		.antMatchers(PUBLIC_MATCHES).permitAll()
 		.anyRequest().authenticated();
+		httpSecurity.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
+	}
+	
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 	
 	@Bean
