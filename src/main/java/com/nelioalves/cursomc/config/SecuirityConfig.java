93c06ef35a302ad249684c81cbcdc3c300106804
nkredd <nel2703@gmail.com>
2019-08-21 16:46:56 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/nelioalves/cursomc/config/SecuirityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecuirityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -25,6 +26,7 @@ import com.nelioalves.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecuirityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
@@ -40,9 +42,13 @@ public class SecuirityConfig extends WebSecurityConfigurerAdapter {
 			"/h2-console/**"			
 	};
 	
-	public static final String[] PUBLIC_MATCHES_GET = { 			
+	public static final String[] PUBLIC_MATCHES_GET = { 		
 			"/produtos/**", 
-			"/categorias/**",
+			"/categorias/**"
+			
+	};
+	
+	public static final String[] PUBLIC_MATCHES_POST = { 
 			"/clientes/**"
 	};
 
@@ -56,6 +62,7 @@ public class SecuirityConfig extends WebSecurityConfigurerAdapter {
 		
 		httpSecurity.cors().and().csrf().disable();
 		httpSecurity.authorizeRequests()
+		.antMatchers(HttpMethod.POST, PUBLIC_MATCHES_POST).permitAll()
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHES_GET).permitAll()
 		.antMatchers(PUBLIC_MATCHES).permitAll()
 		.anyRequest().authenticated();
